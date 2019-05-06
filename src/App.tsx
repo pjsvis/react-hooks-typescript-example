b9f03682a77198ae8f5c590fbde414a5834fb1c9
Peter John Smith <pjstarifa@gmail.com>
2019-05-06 11:05:35 +0100

    More breadcrumbs Needs sorting out

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,25 @@ import {
 import { BreadcrumbExamples } from "./components/breadcrumbs/breadcrumb-examples";
 import { Breadcrumbs } from "./components/breadcrumbs/breadcrumbs";
 import { User } from "./components/user/user";
-import {BreadCrumb2} from './components/breadcrumb2/breadcrumb2'
+import { BreadCrumb2 } from "./components/breadcrumb2/breadcrumb2";
+import { BreadCrumb3 } from "./components/breadcrumb3/breadcrumb3";
+import { NavCrumbContainer } from "./components/nav-crumb/nav-crumb-styles";
+import { NavCrumbs } from "./components/nav-crumb/nav-crumb";
 
 const App = () => {
   const [isOpen, toggleIsOpen] = React.useState(false);
 
   return (
     <>
-      {/* <Header className="header">
+      <button type="button" className="btn btn-primary">
+        Button
+      </button>
+
+      <div className="container">
+        <h1>This is h1</h1>
+
+      <NavCrumbs />
+        {/* <Header className="header">
         <Sidebar pose={isOpen ? "open" : "closed"}>
           <ToggleButton onClick={() => toggleIsOpen(!isOpen)}>X</ToggleButton>
           {headerItems.map(item => (
@@ -28,15 +39,24 @@ const App = () => {
         </Sidebar>
       </Header> */}
 
-      <div className="container">
+        <h1>This is h1</h1>
+        <BreadCrumb3 />
         <BreadCrumb2 />
-      <User
-        name="Alena Sanders"
-        username="@alenasanders"
-        imageURL="https://randomuser.me/api/portraits/women/57.jpg"
-      />
-        <User className="w-20 mb4" name="Peter J Smith" username="pjsTarifa" imageURL="https://cdn-images-1.medium.com/fit/c/60/60/0*fl5__1m0sKZdF6Tx.jpg" />
-        <Breadcrumbs />
+        {/* <NavCrumbs />
+      
+        <BreadCrumb2 />
+        <User
+          name="Alena Sanders"
+          username="@alenasanders"
+          imageURL="https://randomuser.me/api/portraits/women/57.jpg"
+        />
+        <User
+          className="w-20 mb4"
+          name="Peter J Smith"
+          username="pjsTarifa"
+          imageURL="https://cdn-images-1.medium.com/fit/c/60/60/0*fl5__1m0sKZdF6Tx.jpg"
+        /> */}
+        {/* <Breadcrumbs /> */}
         <BreadcrumbExamples />
         {/* <div><HackerNews /></div> */}
       </div>
---
+++ src/components/nav-crumb/nav-crumb-styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavCrumbContainer = styled.main.attrs({
+  className: "flex b--black-10 br3 mt2"
+})``;
+
+export const NavCrumbItem = styled.article.attrs({
+  className: "w-100 bb b--black-10 pa2 mt2 flex items-center justify-between"
+})``;
+
+
+export const NavCrumbButton = styled.button.attrs({
+  className: "f6 button-reset bg-white ba b--black-10  grow  pointer pv1 black-60",
+  type: "button"
+})``;
